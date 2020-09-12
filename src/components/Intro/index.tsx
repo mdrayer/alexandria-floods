28a769955c4629cfb90a542f0011560596eaf187
Michael Drayer <mdrayer@forumone.com>
2020-09-12 18:12:02 -0400

    Fix typo.

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -10,7 +10,7 @@ const Intro = (): JSX.Element => (
         Using local personal weather stations (PWS), we gathered data dating
         back to October 2012 looking for major precipitation events (note that
         only 2019 and 2020 data are currently in the app). In the charts below,
-        we show the <strong>precipition rate (inch per hour)</strong> and{' '}
+        we show the <strong>precipitation rate (inch per hour)</strong> and{' '}
         <strong>total accumulation (inches)</strong> for selected flood and
         heavy rain events. Flood events use reds/oranges/yellows, while heavy
         rain events use blues/greens.
